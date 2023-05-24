1dfc3e5034646747a8c9e6fe85935a90612c99f0
ForbiddenShadow <forbiddenshadow@protonmail.com>
2023-05-24 21:04:31 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'dark-indigo': '#27374D',
+        'navy-blue': '#526D82',
+        'notgrey-grey': '#9DB2BF',
+        'aliexpress-white': '#DDE6ED',
+      },
+      
+      fontFamily: {
+        'roboto': ["Roboto Mono", 'monospace'],
+        'rubik': ['Rubik', 'sans-serif']
+      },
+
+
+    },
+  },
+  plugins: [],
+}
+
